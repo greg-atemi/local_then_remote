69e29d303394cb8e6f668788ddb87290bbb60188
Greg Atemi <magrega7180@gmail.com>
2023-10-10 15:22:57 +0300

    Initial commit
    Initializing the repository by adding gitignore and summation files

---
+++ summation.py
@@ -0,0 +1,9 @@
+def sum():
+    print("Enter the first number")
+    num1 = int(input())
+    print("Enter the second number")
+    num2 = int(input())
+    sum = num1 + num2
+    print("The sum of " + str(num1) + " and " + str(num2) + " is " + str(sum))
+
+sum()
